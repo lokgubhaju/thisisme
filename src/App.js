788486229a755121bc3a1abb73b850d35d9c7eba
Jose De Freitas <joselocotote26@gmail.com>
2020-08-30 15:58:33 -0500

    💄 Add styles to first section

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
+      <header id="header">
         <h1>thisisme</h1>
         <p>Create cool images containing useful information about you and your work</p>
-        <button>Start</button>
+        <a href="#">Start</a>
       </header>
+      <main id="main">
+        <section id="programming-section" className="section">
+          <h1>Programming languages</h1>
+        </section>
+        <section id="technologies-section" className="section">
+          <h1>Technologies</h1>
+        </section>
+        <section id="education-section" className="section">
+          <h1>Education</h1>
+        </section>
+        <section id="work-section" className="section">
+          <h1>Work</h1>
+        </section>
+        <section id="certificates-section" className="section">
+          <h1>Certificates</h1>
+        </section>
+        <section id="personal-section" className="section">
+          <h1>Personal information</h1>
+        </section>
+        <section id="contact-section" className="section">
+          <h1>Contact buttons</h1>
+        </section>
+      </main>
     </div>
   );
 }
