8c0e8a55fc973ec250e3d19867eb2f815e695440
Jose De Freitas <joselocotote26@gmail.com>
2020-08-31 18:27:12 -0500

    💄 Style navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navigation() {
     return (
         <nav id="navigation">
             <ul>
-                <li><NavLink to="/" exact activeStyle={{ 'color':'#3aad7f' }}>Home</NavLink></li>
-                <li><NavLink to="/s/personal" exact activeStyle={{ 'color':'#3aad7f' }}>Personal</NavLink></li>
-                <li><NavLink to="/s/programming" exact activeStyle={{ 'color':'#3aad7f' }}>Programming</NavLink></li>
-                <li><NavLink to="/s/technologies" exact activeStyle={{ 'color':'#3aad7f' }}>Technologies</NavLink></li>
-                <li><NavLink to="/s/education" exact activeStyle={{ 'color':'#3aad7f' }}>Education</NavLink></li>
-                <li><NavLink to="/s/job" exact activeStyle={{ 'color':'#3aad7f' }}>Job</NavLink></li>
-                <li><NavLink to="/s/certificates" exact activeStyle={{ 'color':'#3aad7f' }}>Certificates</NavLink></li>
-                <li><NavLink to="/s/social" exact activeStyle={{ 'color':'#3aad7f' }}>Social</NavLink></li>
+                <li><NavLink className="link" to="/" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Home</NavLink></li>
+                <li><NavLink className="link" to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
+                <li><NavLink className="link" to="/s/programming" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Programming</NavLink></li>
+                <li><NavLink className="link" to="/s/technologies" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Technologies</NavLink></li>
+                <li><NavLink className="link" to="/s/education" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Education</NavLink></li>
+                <li><NavLink className="link" to="/s/job" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Job</NavLink></li>
+                <li><NavLink className="link" to="/s/certificates" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Certificates</NavLink></li>
+                <li><NavLink className="link" to="/s/social" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Social</NavLink></li>
             </ul>
         </nav>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
         <header id="header">
             <h1>thisisme</h1>
             <p>Create cool images containing useful<br/>information about you and your work<br/>for your GitHub readme</p>
-            <Link to="/programming">Start</Link>
+            <Link to="/s/personal">Start</Link>
         </header>
     );
 }
