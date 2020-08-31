a25e0a81fb3651aba974f257dda5aa493f0c7927
Jose De Freitas <joselocotote26@gmail.com>
2020-08-31 11:17:47 -0500

    🎨 Add Section component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import Section from'./components/Section';
+
 function App() {
   return (
     <div className="App">
@@ -10,30 +12,7 @@ function App() {
         <a href="#">Start</a>
       </header>
       <main id="main">
-        <section id="programming-section" className="section">
-          <h1>Programming languages</h1>
-        </section>
-        <section id="technologies-section" className="section">
-          <h1>Technologies</h1>
-        </section>
-        <section id="education-section" className="section">
-          <h1>Education</h1>
-        </section>
-        <section id="work-section" className="section">
-          <h1>Work</h1>
-        </section>
-        <section id="certificates-section" className="section">
-          <h1>Certificates</h1>
-        </section>
-        <section id="personal-section" className="section">
-          <h1>Personal information</h1>
-        </section>
-        <section id="contact-section" className="section">
-          <h1>Contact links</h1>
-        </section>
-        <section id="finishing" className="section">
-          <h1>This is how your image(s) look(s):</h1>
-        </section>
+        <Section/>
       </main>
     </div>
   );
