d0da9128ccf0e2cdec02d597aefac11e5e7c9a06
Jose De Freitas <joselocotote26@gmail.com>
2020-08-31 18:34:02 -0500

    💄 Separate home button from main list

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,15 +4,17 @@ import { NavLink } from 'react-router-dom';
 function Navigation() {
     return (
         <nav id="navigation">
+            <div>
+                <NavLink to="/" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Home</NavLink>
+            </div>
             <ul>
-                <li><NavLink className="link" to="/" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Home</NavLink></li>
-                <li><NavLink className="link" to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
-                <li><NavLink className="link" to="/s/programming" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Programming</NavLink></li>
-                <li><NavLink className="link" to="/s/technologies" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Technologies</NavLink></li>
-                <li><NavLink className="link" to="/s/education" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Education</NavLink></li>
-                <li><NavLink className="link" to="/s/job" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Job</NavLink></li>
-                <li><NavLink className="link" to="/s/certificates" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Certificates</NavLink></li>
-                <li><NavLink className="link" to="/s/social" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Social</NavLink></li>
+                <li><NavLink to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
+                <li><NavLink to="/s/programming" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Programming</NavLink></li>
+                <li><NavLink to="/s/technologies" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Technologies</NavLink></li>
+                <li><NavLink to="/s/education" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Education</NavLink></li>
+                <li><NavLink to="/s/job" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Job</NavLink></li>
+                <li><NavLink to="/s/certificates" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Certificates</NavLink></li>
+                <li><NavLink to="/s/social" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Social</NavLink></li>
             </ul>
         </nav>
     );
