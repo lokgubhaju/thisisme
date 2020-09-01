5e1466af2e41ffb97d99eb39de58f73c6bc1f318
Jose De Freitas <joselocotote26@gmail.com>
2020-08-31 21:18:56 -0500

    💄 Change link padding

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,13 +8,13 @@ function Navigation() {
                 <NavLink to="/" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Home</NavLink>
             </div>
             <ul>
-                <li><NavLink to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
+                <li><NavLink className="link-1" to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
                 <li><NavLink to="/s/programming" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Programming</NavLink></li>
                 <li><NavLink to="/s/technologies" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Technologies</NavLink></li>
                 <li><NavLink to="/s/education" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Education</NavLink></li>
                 <li><NavLink to="/s/job" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Job</NavLink></li>
                 <li><NavLink to="/s/certificates" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Certificates</NavLink></li>
-                <li><NavLink to="/s/social" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Social</NavLink></li>
+                <li><NavLink className="link-2" to="/s/social" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Social</NavLink></li>
             </ul>
         </nav>
     );
