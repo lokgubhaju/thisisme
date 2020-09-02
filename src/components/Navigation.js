a9049da4671fe866649dc3ee2748be01be9c8f6e
Jose De Freitas <joselocotote26@gmail.com>
2020-09-02 17:42:32 -0500

    💄 Change navigation bar style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,18 +4,18 @@ import { NavLink } from 'react-router-dom';
 function Navigation() {
     return (
         <nav id="navigation">
-            <div>
-                <NavLink to="/" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Home</NavLink>
-            </div>
             <ul>
-                <li><NavLink className="link-1" to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
+                <li><NavLink to="/" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 54%)','color':'hsl(0, 0%, 100%)' }}>Home</NavLink></li>
+                <li><hr/></li>
+                <li><NavLink to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
                 <li><NavLink to="/s/programming" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Programming</NavLink></li>
                 <li><NavLink to="/s/technologies" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Technologies</NavLink></li>
                 <li><NavLink to="/s/education" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Education</NavLink></li>
                 <li><NavLink to="/s/job" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Job</NavLink></li>
                 <li><NavLink to="/s/certificates" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Certificates</NavLink></li>
                 <li><NavLink to="/s/social" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Social</NavLink></li>
-                <li><NavLink className="link-2" to="/s/result" exact activeStyle={{ 'backgroundColor':'hsl(123, 72%, 48%)','color':'hsl(0, 0%, 100%)' }}>Result</NavLink></li>
+                <li><hr/></li>
+                <li><NavLink to="/s/result" exact activeStyle={{ 'backgroundColor':'hsl(123, 72%, 48%)','color':'hsl(0, 0%, 100%)' }}>Result</NavLink></li>
             </ul>
         </nav>
     );
