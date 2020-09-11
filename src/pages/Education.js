6a946a39c03bba3ba96d88f668f4f724f9414799
Jose De Freitas <joselocotote26@gmail.com>
2020-09-11 13:07:36 -0500

    ✨ Add form state functionality
    * Both in Education and Job sections, there're two forms that you can select to print in the inner cards
    * Create conditionals to automatically layout items in the SVG image when detects changes
    * Style and added colors to form aside elements and notes

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -26,6 +26,8 @@ function Education() {
     const educationYearsPositionTitle1 = (valueEducationName1 === '' && valueEducationTitle1 === '') ? 68 : 144 && (valueEducationTitle1 === '') ? 106 : 144 && (educationTitlePositionTitle1 === 68) ? 106 : 144;
     const educationYearsPositionText1 = (valueEducationName1 === '' && valueEducationTitle1 === '') ? 84 : 160 && (valueEducationTitle1 === '') ? 122 : 160 && (educationTitlePositionText1 === 84) ? 122 : 160;
 
+    const educationInnerCard1 = (valueEducationName1 !== '' || valueEducationTitle1 !== '' || valueEducationYears1 !== '') ? <rect x="25" y="52" width="215px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/> : null;
+
     const educationNameText1 = (valueEducationName1 === '') ? null : <text x="30" y="67"><tspan x="30" y="68" fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">College name</tspan><tspan x="30" y="84" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationName1}</tspan></text>;
     const educationTitleText1 = (valueEducationTitle1 === '') ? null : <text x="30" y="67"><tspan x="30" y={educationTitlePositionTitle1} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career title</tspan><tspan x="30" y={educationTitlePositionText1} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationTitle1}</tspan></text>;
     const educationYearsText1 = (valueEducationYears1 === '') ? null : <text x="30" y="67"><tspan x="30" y={educationYearsPositionTitle1} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career years</tspan><tspan x="30" y={educationYearsPositionText1} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationYears1}</tspan></text>;
@@ -50,22 +52,25 @@ function Education() {
     const educationYearsPositionTitle2 = (valueEducationName2 === '' && valueEducationTitle2 === '') ? 68 : 144 && (valueEducationTitle2 === '') ? 106 : 144 && (educationTitlePositionTitle2 === 68) ? 106 : 144;
     const educationYearsPositionText2 = (valueEducationName2 === '' && valueEducationTitle2 === '') ? 84 : 160 && (valueEducationTitle2 === '') ? 122 : 160 && (educationTitlePositionText2 === 84) ? 122 : 160;
 
-    const educationNameText2 = (valueEducationName2 === '') ? null : <text x="258" y="67"><tspan x="258" y="68" fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">College name</tspan><tspan x="258" y="84" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationName2}</tspan></text>;
-    const educationTitleText2 = (valueEducationTitle2 === '') ? null : <text x="258" y="67"><tspan x="258" y={educationTitlePositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career title</tspan><tspan x="258" y={educationTitlePositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationTitle2}</tspan></text>;
-    const educationYearsText2 = (valueEducationYears2 === '') ? null : <text x="258" y="67"><tspan x="258" y={educationYearsPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career years</tspan><tspan x="258" y={educationYearsPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationYears2}</tspan></text>;
+    const educationInnerCardPosition2 = (educationInnerCard1 === null) ? <rect x="25" y="52" width="215px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/> : <rect x="253" y="52" width="215px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/>;
+    const educationInnerCard2 = (valueEducationName2 !== '' || valueEducationTitle2 !== '' || valueEducationYears2 !== '') ? educationInnerCardPosition2 : null;
+
+    const educationNameText2 = (valueEducationName2 === '') ? null : <text x="258" y="67"><tspan x="258" y="68" fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">College name</tspan><tspan x="258" y="84" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationName2}</tspan></text> && (educationInnerCard1 === null) ? <text x="30" y="67"><tspan x="30" y="68" fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">College name</tspan><tspan x="30" y="84" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationName2}</tspan></text> : <text x="258" y="67"><tspan x="258" y="68" fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">College name</tspan><tspan x="258" y="84" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationName2}</tspan></text>;
+    const educationTitleText2 = (valueEducationTitle2 === '') ? null : <text x="258" y="67"><tspan x="258" y={educationTitlePositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career title</tspan><tspan x="258" y={educationTitlePositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationTitle2}</tspan></text> && (educationInnerCard1 === null) ? <text x="30" y="67"><tspan x="30" y={educationTitlePositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career title</tspan><tspan x="30" y={educationTitlePositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationTitle2}</tspan></text> : <text x="258" y="67"><tspan x="258" y={educationTitlePositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career title</tspan><tspan x="258" y={educationTitlePositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationTitle2}</tspan></text>;
+    const educationYearsText2 = (valueEducationYears2 === '') ? null : <text x="258" y="67"><tspan x="258" y={educationYearsPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career years</tspan><tspan x="258" y={educationYearsPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationYears2}</tspan></text> && (educationInnerCard1 === null) ? <text x="30" y="67"><tspan x="30" y={educationYearsPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career years</tspan><tspan x="30" y={educationYearsPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationYears2}</tspan></text> : <text x="258" y="67"><tspan x="258" y={educationYearsPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Career years</tspan><tspan x="258" y={educationYearsPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueEducationYears2}</tspan></text>;
 
     const svgImageEducation = (
         <svg width="495" height={imgHeight} viewBox={`0 0 495 ${imgHeight}`} fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="0.5" y="0.5" width="99%" height="99%" rx="4.5" fill="#FFFEFE" stroke="#E4E2E2"/>
             <text x="25" y="36" fill="#2F80ED" fontFamily="Segoe UI" fontWeight="600" fontSize="18px">My education</text>
             <g>
-                <rect x="25" y="52" width="215px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/>
+                {educationInnerCard1}
                 {educationNameText1}
                 {educationTitleText1}
                 {educationYearsText1}
             </g>
             <g>
-                <rect x="253" y="52" width="215px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/>
+                {educationInnerCard2}
                 {educationNameText2}
                 {educationTitleText2}
                 {educationYearsText2}
@@ -78,34 +83,36 @@ function Education() {
 
     const educationForm1 = (
         <form>
+            <p className="currentform-text">current form: 1</p>
             <div>
-                <label htmlFor="collegename">College name</label><br/>
-                <input onChange={valueChangeEducationName1} value={valueEducationName1} type="text" id="collegename" name="collegename"/>
+                <label htmlFor="collegename1">College name</label><br/>
+                <input onChange={valueChangeEducationName1} value={valueEducationName1} type="text" id="collegename1" name="collegename1"/>
             </div>
             <div>
-                <label htmlFor="careertitle">Career title</label><br/>
-                <input onChange={valueChangeEducationTitle1} value={valueEducationTitle1} type="text" id="careertitle" name="careertitle"/>
+                <label htmlFor="careertitle1">Career title</label><br/>
+                <input onChange={valueChangeEducationTitle1} value={valueEducationTitle1} type="text" id="careertitle1" name="careertitle1"/>
             </div>
             <div>
-                <label htmlFor="careeryears">Career years <span>(MM/YYYY-MM/YYYY) or (YYYY/YYYY)</span></label><br/>
-                <input onChange={valueChangeEducationYears1} value={valueEducationYears1} type="text" id="careeryears" name="careeryears"/>
+                <label htmlFor="careeryears1">Career years <span>(MM/YYYY-MM/YYYY) or (YYYY/YYYY)</span></label><br/>
+                <input onChange={valueChangeEducationYears1} value={valueEducationYears1} type="text" id="careeryears1" name="careeryears1"/>
             </div>
         </form>
     );
 
     const educationForm2 = (
         <form>
+            <p className="currentform-text">current form: 2</p>
             <div>
-                <label htmlFor="collegename">College name</label><br/>
-                <input onChange={valueChangeEducationName2} value={valueEducationName2} type="text" id="collegename" name="collegename"/>
+                <label htmlFor="collegename2">College name</label><br/>
+                <input onChange={valueChangeEducationName2} value={valueEducationName2} type="text" id="collegename2" name="collegename2"/>
             </div>
             <div>
-                <label htmlFor="careertitle">Career title</label><br/>
-                <input onChange={valueChangeEducationTitle2} value={valueEducationTitle2} type="text" id="careertitle" name="careertitle"/>
+                <label htmlFor="careertitle2">Career title</label><br/>
+                <input onChange={valueChangeEducationTitle2} value={valueEducationTitle2} type="text" id="careertitle2" name="careertitle2"/>
             </div>
             <div>
-                <label htmlFor="careeryears">Career years <span>(MM/YYYY-MM/YYYY) or (YYYY/YYYY)</span></label><br/>
-                <input onChange={valueChangeEducationYears2} value={valueEducationYears2} type="text" id="careeryears" name="careeryears"/>
+                <label htmlFor="careeryears2">Career years <span>(MM/YYYY-MM/YYYY) or (YYYY/YYYY)</span></label><br/>
+                <input onChange={valueChangeEducationYears2} value={valueEducationYears2} type="text" id="careeryears2" name="careeryears2"/>
             </div>
         </form>
     );
--- src/pages/Job.js
+++ src/pages/Job.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navigation from '../components/Navigation';
 import useLocalStorage from '../scripts/localStorageData';
@@ -6,35 +6,118 @@ import useLocalStorage from '../scripts/localStorageData';
 function Job() {
     const imgHeight = 194;
 
-    const [valueJobCompany, setValueJobCompany] = useLocalStorage(
-        'jobCompanyValueStored'
+    const [valueJobCompany1, setValueJobCompany1] = useLocalStorage(
+        'jobCompanyValueStored1'
       );
-    const valueChangeJobCompany = event => setValueJobCompany(event.target.value);
+    const valueChangeJobCompany1 = event => setValueJobCompany1(event.target.value);
 
-    const [valueJobPosition, setValueJobPosition] = useLocalStorage(
-        'jobPositionValueStored'
+    const [valueJobPosition1, setValueJobPosition1] = useLocalStorage(
+        'jobPositionValueStored1'
       );
-    const valueChangeJobPosition = event => setValueJobPosition(event.target.value);
+    const valueChangeJobPosition1 = event => setValueJobPosition1(event.target.value);
 
-    const [valueJobYears, setValueJobYears] = useLocalStorage(
-        'jobYearsValueStored'
+    const [valueJobYears1, setValueJobYears1] = useLocalStorage(
+        'jobYearsValueStored1'
       );
-    const valueChangeJobYears = event => setValueJobYears(event.target.value);
+    const valueChangeJobYears1 = event => setValueJobYears1(event.target.value);
 
-    const jobCompanyText = (valueJobCompany === '') ? null : <text x="25" y="36"><tspan x="25" y="36" fill="#2F80ED" fontFamily="Segoe UI" fontWeight="600" fontSize="18px">Company name</tspan><tspan x="25" y="60" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="14px">{valueJobCompany}</tspan></text>;
-    const jobPositionText = (valueJobPosition === '') ? null : <text x="25" y="36"><tspan x="25" y="90" fill="#2F80ED" fontFamily="Segoe UI" fontWeight="600" fontSize="18px">Position</tspan><tspan x="25" y="114" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="14px">{valueJobPosition}</tspan></text>;
-    const jobYearsText = (valueJobYears === '') ? null : <text x="25" y="36"><tspan x="25" y="144" fill="#2F80ED" fontFamily="Segoe UI" fontWeight="600" fontSize="18px">Job years</tspan><tspan x="25" y="168" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="14px">{valueJobYears}</tspan></text>;
+    const jobPositionPositionTitle1 = (valueJobCompany1 === '') ? 68 : 106;
+    const jobPositionPositionText1 = (valueJobCompany1 === '') ? 84 : 122;
+    const jobYearsPositionTitle1 = (valueJobCompany1 === '' && valueJobPosition1 === '') ? 68 : 144 && (valueJobPosition1 === '') ? 106 : 144 && (jobPositionPositionTitle1 === 68) ? 106 : 144;
+    const jobYearsPositionText1 = (valueJobCompany1 === '' && valueJobPosition1 === '') ? 84 : 160 && (valueJobPosition1 === '') ? 122 : 160 && (jobPositionPositionText1 === 84) ? 122 : 160;
+
+    const jobInnerCard1 = (valueJobCompany1 !== '' || valueJobPosition1 !== '' || valueJobYears1 !== '') ? <rect x="25" y="52" width="215px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/> : null;
+
+    const jobCompanyText1 = (valueJobCompany1 === '') ? null : <text x="30" y="67"><tspan x="30" y="68" fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Company name</tspan><tspan x="30" y="84" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobCompany1}</tspan></text>;
+    const jobPositionText1 = (valueJobPosition1 === '') ? null : <text x="30" y="67"><tspan x="30" y={jobPositionPositionTitle1} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Position</tspan><tspan x="30" y={jobPositionPositionText1} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobPosition1}</tspan></text>;
+    const jobYearsText1 = (valueJobYears1 === '') ? null : <text x="30" y="67"><tspan x="30" y={jobYearsPositionTitle1} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Job years</tspan><tspan x="30" y={jobYearsPositionText1} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobYears1}</tspan></text>;
+
+    const [valueJobCompany2, setValueJobCompany2] = useLocalStorage(
+        'jobCompanyValueStored2'
+      );
+    const valueChangeJobCompany2 = event => setValueJobCompany2(event.target.value);
+
+    const [valueJobPosition2, setValueJobPosition2] = useLocalStorage(
+        'jobPositionValueStored2'
+      );
+    const valueChangeJobPosition2 = event => setValueJobPosition2(event.target.value);
+
+    const [valueJobYears2, setValueJobYears2] = useLocalStorage(
+        'jobYearsValueStored2'
+      );
+    const valueChangeJobYears2 = event => setValueJobYears2(event.target.value);
+
+    const jobPositionPositionTitle2 = (valueJobCompany2 === '') ? 68 : 106;
+    const jobPositionPositionText2 = (valueJobCompany2 === '') ? 84 : 122;
+    const jobYearsPositionTitle2 = (valueJobCompany2 === '' && valueJobPosition2 === '') ? 68 : 144 && (valueJobPosition2 === '') ? 106 : 144 && (jobPositionPositionTitle2 === 68) ? 106 : 144;
+    const jobYearsPositionText2 = (valueJobCompany2 === '' && valueJobPosition2 === '') ? 84 : 160 && (valueJobPosition2 === '') ? 122 : 160 && (jobPositionPositionText2 === 84) ? 122 : 160;
+
+    const jobInnerCardPosition2 = (jobInnerCard1 === null) ? <rect x="25" y="52" width="215px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/> : <rect x="253" y="52" width="215px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/>;
+    const jobInnerCard2 = (valueJobCompany2 !== '' || valueJobPosition2 !== '' || valueJobYears2 !== '') ? jobInnerCardPosition2 : null;
+
+    const jobCompanyText2 = (valueJobCompany2 === '') ? null : <text x="258" y="67"><tspan x="258" y="68" fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Company name</tspan><tspan x="258" y="84" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobCompany2}</tspan></text> && (jobInnerCard1 === null) ? <text x="30" y="67"><tspan x="30" y="68" fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Company name</tspan><tspan x="30" y="84" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobCompany2}</tspan></text> : <text x="258" y="67"><tspan x="258" y="68" fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Company name</tspan><tspan x="258" y="84" fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobCompany2}</tspan></text>;
+    const jobPositionText2 = (valueJobPosition2 === '') ? null : <text x="258" y="67"><tspan x="258" y={jobPositionPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Position</tspan><tspan x="258" y={jobPositionPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobPosition2}</tspan></text> && (jobInnerCard1 === null) ? <text x="30" y="67"><tspan x="30" y={jobPositionPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Position</tspan><tspan x="30" y={jobPositionPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobPosition2}</tspan></text> : <text x="258" y="67"><tspan x="258" y={jobPositionPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Position</tspan><tspan x="258" y={jobPositionPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobPosition2}</tspan></text>;
+    const jobYearsText2 = (valueJobYears2 === '') ? null : <text x="258" y="67"><tspan x="258" y={jobYearsPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Job years</tspan><tspan x="258" y={jobYearsPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobYears2}</tspan></text> && (jobInnerCard1 === null) ? <text x="30" y="67"><tspan x="30" y={jobYearsPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Job years</tspan><tspan x="30" y={jobYearsPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobYears2}</tspan></text> : <text x="258" y="67"><tspan x="258" y={jobYearsPositionTitle2} fill="#333" fontFamily="Segoe UI" fontWeight="700" fontSize="11px">Job years</tspan><tspan x="258" y={jobYearsPositionText2} fill="#333" fontFamily="Segoe UI" fontWeight="400" fontSize="12px">{valueJobYears2}</tspan></text>;
 
     const svgImageJob = (
         <svg width="495" height={imgHeight} viewBox={`0 0 495 ${imgHeight}`} fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="0.5" y="0.5" width="99%" height="99%" rx="4.5" fill="#FFFEFE" stroke="#E4E2E2"/>
-            {jobCompanyText}
-            {jobPositionText}
-            {jobYearsText}
+            <text x="25" y="36" fill="#2F80ED" fontFamily="Segoe UI" fontWeight="600" fontSize="18px">My job experience</text>
+            <g>
+                {jobInnerCard1}
+                {jobCompanyText1}
+                {jobPositionText1}
+                {jobYearsText1}
+            </g>
+            <g>
+                {jobInnerCard2}
+                {jobCompanyText2}
+                {jobPositionText2}
+                {jobYearsText2}
+            </g>
             {/*<rect x="25" y="52" width="140px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/>
             <rect x="175" y="52" width="140px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/>
             <rect x="325" y="52" width="140px" height="118px" rx="4.5" fill="#FAFBFC" stroke="#E4E2E2"/>*/}
          </svg>
+    );
+
+    const [form, changeForm] = useState(false);
+    const toggleForm = () => changeForm(!form);
+
+    const jobForm1 = (
+        <form>
+            <p className="currentform-text">current form: 1</p>
+            <div>
+                <label htmlFor="companyname">Company name</label><br/>
+                <input onChange={valueChangeJobCompany1} value={valueJobCompany1} type="text" id="companyname" name="companyname"/>
+            </div>
+            <div>
+                <label htmlFor="position">Position</label><br/>
+                <input onChange={valueChangeJobPosition1} value={valueJobPosition1} type="text" id="position" name="position"/>
+            </div>
+            <div>
+                <label htmlFor="jobyears">Job years <span>(MM/YYYY-MM/YYYY)</span></label><br/>
+                <input onChange={valueChangeJobYears1} value={valueJobYears1} type="text" id="jobyears" name="jobyears"/>
+            </div>
+        </form>
+    );
+
+    const jobForm2 = (
+        <form>
+            <p className="currentform-text">current form: 2</p>
+            <div>
+                <label htmlFor="companyname">Company name</label><br/>
+                <input onChange={valueChangeJobCompany2} value={valueJobCompany2} type="text" id="companyname" name="companyname"/>
+            </div>
+            <div>
+                <label htmlFor="position">Position</label><br/>
+                <input onChange={valueChangeJobPosition2} value={valueJobPosition2} type="text" id="position" name="position"/>
+            </div>
+            <div>
+                <label htmlFor="jobyears">Job years <span>(MM/YYYY-MM/YYYY)</span></label><br/>
+                <input onChange={valueChangeJobYears2} value={valueJobYears2} type="text" id="jobyears" name="jobyears"/>
+            </div>
+        </form>
     );
 
     return (
@@ -44,20 +127,9 @@ function Job() {
                 <div>
                     <section id="section">
                         <div id="form-wrapper">
-                            <form>
-                                <div>
-                                    <label htmlFor="companyname">Company name</label><br/>
-                                    <input onChange={valueChangeJobCompany} value={valueJobCompany} type="text" id="companyname" name="companyname"/>
-                                </div>
-                                <div>
-                                    <label htmlFor="position">Position</label><br/>
-                                    <input onChange={valueChangeJobPosition} value={valueJobPosition} type="text" id="position" name="position"/>
-                                </div>
-                                <div>
-                                    <label htmlFor="jobyears">Job years <span>(MM/YYYY-MM/YYYY)</span></label><br/>
-                                    <input onChange={valueChangeJobYears} value={valueJobYears} type="text" id="jobyears" name="jobyears"/>
-                                </div>
-                            </form>
+                            {form ? jobForm2 : jobForm1}
+                            <button id="toggle-button" onClick={toggleForm}>Toggle form</button>
+                            <p> each form corresponds to each inner card.</p>
                         </div>
                     </section>
                     <aside id="section-aside">
