d64e6baaf285f2a0d19100b636f1cd934c1d9ec7
Jose De Freitas <joselocotote26@gmail.com>
2020-09-15 21:05:41 -0500

    🎨 Rearrange Personal position

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import Personal from './pages/Personal';
 import Programming from './pages/Programming';
 import Technologies from './pages/Technologies';
 import Education from './pages/Education';
 import Job from './pages/Job';
+import Personal from './pages/Personal';
 import Certificates from './pages/Certificates';
 import Result from './pages/Result';
 import Privacy from './pages/Privacy';
@@ -18,11 +18,11 @@ function App() {
         <>
           <Switch>
             <Route exact path="/" component={ Home }/>
-            <Route exact path="/s/personal" component={ Personal }/>
             <Route exact path="/s/programming" component={ Programming }/>
             <Route exact path="/s/technologies" component={ Technologies }/>
             <Route exact path="/s/education" component={ Education }/>
             <Route exact path="/s/job" component={ Job }/>
+            <Route exact path="/s/personal" component={ Personal }/>
             <Route exact path="/s/certificates" component={ Certificates }/>
             <Route exact path="/s/result" component={ Result }/>
             <Route exact path="/privacy-policy" component={ Privacy }/>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,11 +6,11 @@ function Navigation() {
         <nav id="navigation">
             <ul>
                 <li><NavLink id="home-button" to="/" exact>Home</NavLink></li>
-                <li><NavLink to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
                 <li><NavLink to="/s/programming" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Programming</NavLink></li>
                 <li><NavLink to="/s/technologies" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Technologies</NavLink></li>
                 <li><NavLink to="/s/education" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Education</NavLink></li>
                 <li><NavLink to="/s/job" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Job</NavLink></li>
+                <li><NavLink to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
                 <li><NavLink to="/s/certificates" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Certificates</NavLink></li>
                 <li><hr id="separator"/></li>
                 <li><NavLink to="/s/result" exact activeStyle={{ 'backgroundColor':'hsl(125, 75%, 41%)','color':'hsl(0, 0%, 100%)' }}>Result</NavLink></li>
