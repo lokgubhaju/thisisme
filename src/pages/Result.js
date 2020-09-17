f41562965281b332c0ade38289002f452243cad3
Jose De Freitas <joselocotote26@gmail.com>
2020-09-17 12:45:11 -0500

    💩 Try storing svg images on local storage

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-import { SvgImageProgramming } from './Programming';
 
 function Result() {
     return (
         <>
             <Navigation/>
             <section id="section">
-                <SvgImageProgramming/>
+                {localStorage.getItem('svgImageProgrammingStored')}
             </section>
         </>
     );
