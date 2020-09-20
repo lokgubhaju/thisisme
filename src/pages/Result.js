9957ed605d33c5b79dee3320371cdad8e4972b13
Jose De Freitas <joselocotote26@gmail.com>
2020-09-20 14:47:04 -0500

    🎨 Import two svg images

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 import parse from 'html-react-parser';
+import DOMPurify from 'dompurify';
 
 function Result() {
     const svgImageProgrammingPassed = localStorage.getItem('svgImageProgrammingStored')
     const svgImageTechnologiesPassed = localStorage.getItem('svgImageTechnologiesStored')
+
+    const svgImageProgrammingPassedClean = DOMPurify.sanitize(svgImageProgrammingPassed);
+    const svgImageTechnologiesPassedClean = DOMPurify.sanitize(svgImageTechnologiesPassed);
+
     return (
         <>
             <Navigation/>
             <svg width="900" height="192" viewBox="0 0 900 192" fill="none" xmlns="http://www.w3.org/2000/svg">
-                {parse(svgImageProgrammingPassed)}
-                {parse(svgImageTechnologiesPassed)}
+                {parse(svgImageProgrammingPassedClean)}
+                {parse(svgImageTechnologiesPassedClean)}
             </svg>
         </>
     );
